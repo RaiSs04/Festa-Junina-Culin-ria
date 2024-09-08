b28c94e8a51ccab71203e9ed4c61dba5fbe1af98
RaiSs04 <145294002+RaiSs04@users.noreply.github.com>
2024-09-07 23:36:01 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        titulo: "Pamonha",
+        descricao: "Ingredientes: Milho verde, leite de coco, sal, cheiro-verde.</br>Preparo: Bata o milho no liquidificador com um pouco de água. Adicione o leite de coco, sal e cheiro-verde a gosto. Envolva em palha de milho e cozinhe em água fervente por cerca de 40 minutos.",
+        link: "https://pt.wikipedia.org/wiki/Pamonha",
+        tags: "pamonha milho "
+    },
+    {
+        titulo: "Curau",
+        descricao: "Ingredientes: Milho triturado, leite de coco, açúcar, canela em pó.</br>Preparo: Cozinhe o milho triturado em leite de coco até engrossar. Adicione açúcar e canela a gosto. Sirva quente ou frio.",
+        link: "https://pt.wikipedia.org/wiki/Curau",
+        tags: "curau"
+    },
+    {
+        titulo: "Canjica",
+        descricao: "Ingredientes: Grãos de milho branco, leite de coco, açúcar, canela em pó, cravo.</br>Preparo: Cozinhe os grãos de milho em água até amaciarem. Adicione o leite de coco, açúcar, canela e cravo. Cozinhe até engrossar.",
+        link: "https://pt.wikipedia.org/wiki/Canjica",
+        tags: "canjica"
+    },
+    {
+        titulo: "Bolo de Milho",
+        descricao: "Ingredientes: Milho verde, fubá, açúcar, ovos, leite, óleo, fermento em pó e queijo ralado.Modo de </br>preparo: Bata no liquidificador os ingredientes líquidos. Em uma tigela, misture os ingredientes secos e adicione os líquidos aos poucos. Asse em forno pré-aquecido até dourar.",
+        link: "https://pt.wikipedia.org/wiki/Bolo_de_milho",
+        tags: "bolo de milho"
+    },
+    {
+        titulo: "Arroz-doce",
+        descricao: "Ingredientes: Arroz, leite, açúcar, canela em pó e raspas de limão.</br>Preparo: Cozinhe o arroz no leite com o açúcar até ficar cremoso. Adicione a canela e as raspas de limão.",
+        link: "https://pt.wikipedia.org/wiki/Arroz-doce",
+        tags: "arroz doce"
+    },
+    {
+        titulo: "Paçoca",
+        descricao: "Ingredientes: Amendoim torrado e moído, açúcar e manteiga.</br>Preparo: Misture todos os ingredientes até formar uma massa homogênea. Modele em pequenas porções.",
+        link: "https://pt.wikipedia.org/wiki/Pa%C3%A7oca_de_amendoim",
+        tags: "paçoca"
+    },
+];
